cfe475e608c47eb8d823ffd2cc0f11ccb2afa7c6
pvherreramusic <Trachkid@gmail.com>
2021-02-01 11:28:53 -0800

    Photo  update

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Tab } from 'semantic-ui-react'
-import { Music, Homepage , JavaSc} from "./index";
+import { Music, Homepage , JavaSc, ImageExampleGroupSize} from "./index";
 
 const panes = [
   { menuItem: 'Home/News', render: () => <Homepage></Homepage>},
   { menuItem: 'Music Content', render: () => <Music></Music> },
-  { menuItem: 'JavaScript Projects', render: () => <JavaSc></JavaSc>}
-
+  { menuItem: 'JavaScript Projects', render: () => <JavaSc></JavaSc>},
+  { menuItem: 'Pictures of P.V. Herrera by his Friends', render: () => <ImageExampleGroupSize></ImageExampleGroupSize>}
   
 ]
 
