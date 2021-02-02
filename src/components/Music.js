481a7e4dfc220a1ac400e3e65dc6607ae5c5ce17
pvherreramusic <Trachkid@gmail.com>
2021-02-01 20:20:29 -0800

    Added Soundcloud Tracks

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, Image } from "semantic-ui-react";
+import { Grid, Image, GridColumn } from "semantic-ui-react";
 import Iframe from 'react-iframe'
 
 const Music = () => (
@@ -9,7 +9,7 @@ const Music = () => (
 <h1>WELCOME TO PV HERRERA MUSIC</h1>
 <h2>PLEASE, ENJOY THE MUSIC</h2>
 
-  <Grid columns={2}>
+  <Grid columns={3}>
   <Grid.Row>
   <Grid.Column width={4}>
 
@@ -53,10 +53,25 @@ const Music = () => (
         position="relative"/>
   </Grid.Column>
   </Grid.Row>
-</Grid>
+
+  <Grid.Row>
+  <Grid.Column width={4}>
+
+ <p width="520px">Linear Sound Instr. LP 3</p>
+
+  </Grid.Column>
+  <Grid.Column width={6}>
+  <Image src='https://f4.bcbits.com/img/a2685089535_10.jpg' width="520px"
+        height="320px" />
+
+  </Grid.Column>
+  </Grid.Row>
 
 
-<h1>Soundcloud Tracks</h1>
+  <h1>Soundcloud Tracks</h1>
+  <Grid.Row>
+<Grid.Column width={6}>
+
 <Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/949854043&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
         width="320px"
         height="320px"
@@ -64,9 +79,37 @@ const Music = () => (
         className="myClassname"
         display="initial"
         position="relative"/>
+      
+       
+</Grid.Column>
 
+<Grid.Column width={6}>
 
+<Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/966663577&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        width="320px"
+        height="320px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
+      
+       
+</Grid.Column>
 
+<Grid.Column width={4}>
+
+<Iframe url="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/966666784&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"
+        width="320px"
+        height="320px"
+        id="myId"
+        className="myClassname"
+        display="initial"
+        position="relative"/>
+      
+       
+</Grid.Column>
+</Grid.Row>
+</Grid>
 </div>
 
 )
